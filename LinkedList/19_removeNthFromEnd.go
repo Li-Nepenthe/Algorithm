425da0c1141cc3216a0396a37a3ee9ee8c9e562b
Nepenthe <651483313@qq.com>
2026-03-18 14:53:30 +0800

    完成力扣第19题

--- LinkedList/19_removeNthFromEnd.go
+++ LinkedList/19_removeNthFromEnd.go
@@ -8,6 +8,17 @@ package LinkedList
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-
-	return nil
+	dummy := &ListNode{Next: head}
+	prev := dummy
+	// 找寻倒数n+1节点
+	for i := 0; i < n && prev.Next != nil; i++ {
+		prev = prev.Next
+	}
+	curr := dummy
+	for prev.Next != nil {
+		curr = curr.Next
+		prev = prev.Next
+	}
+	curr.Next = curr.Next.Next
+	return dummy.Next
 }
